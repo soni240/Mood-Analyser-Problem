ebde9edce9f51d0d39bf1ff02bfe15269166c7a3
soni240 <s.fegade21@gmail.com>
2022-05-24 15:31:54 +0530

    [Gunavanti]Handle_Exception

---
+++ UC2_Handle_Exception/MoodAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_Handle_Exception
+{
+    internal class MoodAnalyzer
+    {
+        string message;
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+
+                if (message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                    Console.WriteLine("i am in happy mood");
+                }
+                else
+                {
+                    return "sad";
+                    Console.WriteLine("i am in sad mood");
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "happy";
+
+            }
+        }
+    }
+}
+
+    
+
--- UC2_Handle_Exception/Program.cs
+++ UC2_Handle_Exception/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC1_Test_Case
+namespace MoodAnalyserProb
 {
     class Program
     {
